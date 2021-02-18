244cafcd4dd03ee75a04cec9531e6898d45e8dd9
Ben Crossman <bencrossman@gmail.com>
2021-02-18 18:26:02 +0800

    Batch files

---
+++ extras/AudioPluginHost/Live audio mode.bat
@@ -0,0 +1,25 @@
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved" /f
+
+pskill agent_ovpnconnect_1603802913401.exe
+pskill Calculator.exe
+pskill EpicGamesLauncher.exe
+pskill GoogleCrashHandler.exe
+pskill GoogleCrashHandler64.exe
+pskill googledrivesync.exe
+pskill Microsoft.Photos.exe
+pskill slack.exe
+pskill spoolsv.exe
+pskill sqlwriter.exe
+pskill Teams.exe
+pskill TeamViewer.exe
+pskill TeamViewer_Service.exe
+pskill TSVNCache.exe
+pskill tv_w32.exe
+pskill tv_x64.exe
+pskill UnrealCEFSubProcess.exe
+pskill Video.UI.exe
+pskill vpnagent.exe
+pskill vpnui.exe
+pskill winampa.exe
+pskill WinStore.App.exe
+pskill YourPhone.exe
