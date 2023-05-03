7693328d52492df7f201457f810a8b14aaaf6d41
Ben Crossman <bencrossman@gmail.co>
2023-05-03 19:21:38 +1000

    Loading messages

--- extras/AudioPluginHost/Source/Plugins/PluginGraph.cpp
+++ extras/AudioPluginHost/Source/Plugins/PluginGraph.cpp
@@ -504,6 +504,7 @@ void PluginGraph::setupPerformer()
 			AddRack(processor, rack);
         }
     }
+    m_keylabReady = true;
 }
 
 File PluginGraph::getLastDocumentOpened()
@@ -790,8 +791,6 @@ void PluginGraph::LoadSet(int setIndex)
 }
     }
 
-bool m_keylabNeedsSettingup = true;
-
 void PluginGraph::Filter(int samples, int sampleRate, MidiBuffer &midiBuffer)
 {
     if (m_keylabNeedsSettingup)
@@ -799,8 +798,17 @@ void PluginGraph::Filter(int samples, int sampleRate, MidiBuffer &midiBuffer)
         m_keylabNeedsSettingup = false;
         if (!m_isKeylab88MkII) // We'll use "Analog Lab" mode so no controller assignment, remap at runtime
             SetupKeylab(midiBuffer, 0);
-        PrintLCDScreen(midiBuffer, 0, "Engine Loaded", "Select setlist");
+        PrintLCDScreen(midiBuffer, 0, "Performer", "Loading...");
     }
+
+    if (m_keylabReady)
+    {
+        MidiBuffer output;
+        m_keylabReady = false;
+        UpdateLCDScreen(output, 0, m_performer.m_currentPerformanceIndex);
+        midiBuffer = output;
+    }
+
     samples;
     sampleRate;
 
