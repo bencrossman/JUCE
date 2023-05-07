6dbcabe12a2abba5dccd4ad81214c9d5af277ba5
Ben Crossman <bencrossman@gmail.co>
2023-05-07 16:09:41 +1000

    Undo plugin list resize change

--- extras/AudioPluginHost/Source/UI/MainHostWindow.cpp
+++ extras/AudioPluginHost/Source/UI/MainHostWindow.cpp
@@ -274,7 +274,7 @@ public:
                                                         getAppProperties().getUserSettings(),
                                                         true), true);
 
-        setResizable (false, false);
+        setResizable (true, false);
         setResizeLimits (300, 400, 800, 1500);
         setTopLeftPosition (60, 60);
 
