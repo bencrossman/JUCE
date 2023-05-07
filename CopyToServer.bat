036d4683d2505ddec59119469b18038f85a42272
Ben Crossman <bencrossman@gmail.co>
2023-05-07 22:34:22 +1000

    Batch file, Kill P8?

--- CopyToServer.bat
+++ CopyToServer.bat
@@ -1,1 +1,2 @@
 copy "extras\AudioPluginHost\Builds\VisualStudio2019\x64\Release\App\AudioPluginHost.exe" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
+copy "extras\AudioPluginHost\Ben.performer" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
