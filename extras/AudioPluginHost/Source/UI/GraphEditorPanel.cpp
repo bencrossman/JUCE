1a0a0c51037f75fba35d4b6fd80bf313cf66d523
Ben Crossman <bencrossman@gmail.co>
2023-05-07 17:04:20 +1000

    Just fire ChangeListener once for now (Resize will still work)

--- extras/AudioPluginHost/Source/UI/GraphEditorPanel.cpp
+++ extras/AudioPluginHost/Source/UI/GraphEditorPanel.cpp
@@ -805,8 +805,15 @@ void GraphEditorPanel::resized()
     updateComponents();
 }
 
+static bool changeListenerCallbackDone = false;
+
 void GraphEditorPanel::changeListenerCallback (ChangeBroadcaster*)
 {
+    if (changeListenerCallbackDone)
+        return;
+
+    changeListenerCallbackDone = true;
+
     updateComponents();
 }
 
