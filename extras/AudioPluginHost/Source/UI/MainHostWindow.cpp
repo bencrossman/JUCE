41a43084842c18ddb1094f0d3ea87bc7a946e685
Ben Crossman <bencrossman@gmail.co>
2023-05-07 16:03:36 +1000

    Scale rack region to bottom of screen, resize off, dont restore size

--- extras/AudioPluginHost/Source/UI/MainHostWindow.cpp
+++ extras/AudioPluginHost/Source/UI/MainHostWindow.cpp
@@ -308,9 +308,9 @@ MainHostWindow::MainHostWindow()
    #if JUCE_IOS || JUCE_ANDROID
     setFullScreen (true);
    #else
-    setResizable (true, false);
-    setResizeLimits (500, 400, 10000, 10000);
-    centreWithSize (1024, 720);
+    setResizable (false, false);
+    //setResizeLimits (500, 400, 10000, 10000);
+    centreWithSize (966, getParentHeight() - 62); // minus Windows task bar but also not quite working so just a value to make sure entire window visible
    #endif
 
     knownPluginList.setCustomScanner (std::make_unique<CustomPluginScanner>());
@@ -321,7 +321,8 @@ MainHostWindow::MainHostWindow()
 
     setUsingNativeTitleBar (true);
 
-    restoreWindowStateFromString (getAppProperties().getUserSettings()->getValue ("mainWindowPos"));
+    //restoreWindowStateFromString (getAppProperties().getUserSettings()->getValue ("mainWindowPos")); seemed to keep going fullscreen
+
 
     setVisible (true);
 
