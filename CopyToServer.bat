e278370505034d9f5543d00e0994423c435268e0
Ben Crossman <bencrossman@gmail.co>
2023-05-30 21:55:27 +1000

    Playing with some new song sounds. Undid changes to AllNotesOff (Arp bug)

--- CopyToServer.bat
+++ CopyToServer.bat
@@ -1,2 +1,4 @@
 copy "extras\AudioPluginHost\Builds\VisualStudio2019\x64\Release\App\AudioPluginHost.exe" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
 copy "extras\AudioPluginHost\Ben.performer" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
+copy "extras\AudioPluginHost\Sampler.txt" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
+
