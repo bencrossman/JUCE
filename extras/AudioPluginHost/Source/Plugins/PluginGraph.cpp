1321463e280badc55a3505e3bb633b5ecb1230c1
Ben Crossman <bencrossman@gmail.co>
2023-08-21 22:06:13 +1000

    Piano doesn't need PC's. Mute while we are sending global chunks (otherwise guitar will kick off)
    # Conflicts:
    #	extras/AudioPluginHost/Ben.performer

--- extras/AudioPluginHost/Source/Plugins/PluginGraph.cpp
+++ extras/AudioPluginHost/Source/Plugins/PluginGraph.cpp
@@ -765,6 +765,9 @@ void PluginGraph::setupPerformer()
         }
     }
 
+    // Mute while we are sending global chunks (otherwise guitar will kick off)
+    ((AudioProcessorGraph::Node*)(m_masterGainNode))->getProcessor()->getParameters()[0]->setValue(0);
+
     for (auto i = 0U; i < m_performer.Root.Racks.Rack.size(); ++i)
     {
         if (m_performer.Root.Racks.Rack[i].m_node && m_performer.Root.Racks.Rack[i].InitialState.size())
@@ -773,6 +776,9 @@ void PluginGraph::setupPerformer()
             SendChunkString(processor, m_performer.Root.Racks.Rack[i].InitialState);
         }
     }
+
+    ((AudioProcessorGraph::Node*)(m_masterGainNode))->getProcessor()->getParameters()[0]->setValue(0.25f);
+
 
     m_initializing = false;
 
