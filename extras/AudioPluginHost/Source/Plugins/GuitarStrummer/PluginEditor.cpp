7edac2c4c2f3559bcad087275a064544ca465df4
Ben Crossman <bencrossman@gmail.co>
2024-01-01 21:45:39 +1000

    Test putting guitar strummer as internal plugin

---
+++ extras/AudioPluginHost/Source/Plugins/GuitarStrummer/PluginEditor.cpp
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+GuitarStrummerAudioProcessorEditor::GuitarStrummerAudioProcessorEditor(GuitarStrummerAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+
