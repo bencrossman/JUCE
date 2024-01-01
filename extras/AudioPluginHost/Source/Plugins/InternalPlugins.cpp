17832da085ab3a6122f47c89469b81efa406adfa
Ben Crossman <bencrossman@gmail.co>
2024-01-01 21:45:39 +1000

    Test putting guitar strummer as internal plugin

--- extras/AudioPluginHost/Source/Plugins/InternalPlugins.cpp
+++ extras/AudioPluginHost/Source/Plugins/InternalPlugins.cpp
@@ -38,6 +38,7 @@
 
 #include "InternalPlugins.h"
 #include "PluginGraph.h"
+#include "GuitarStrummer/PluginProcessor.h"
 
 #define PIP_DEMO_UTILITIES_INCLUDED 1
 
@@ -525,8 +526,9 @@ InternalPluginFormat::InternalPluginFormat()
         //[] { return std::make_unique<InternalPlugin> (std::make_unique<MultiOutSynth>()); },
         [] { return std::make_unique<InternalPlugin> (std::make_unique<NoiseGate>()); },
         [] { return std::make_unique<InternalPlugin> (std::make_unique<SamplerAudioProcessor>()); },
-        [] { return std::make_unique<InternalPlugin> (std::make_unique<SurroundProcessor>()); }
-    }
+        [] { return std::make_unique<InternalPlugin> (std::make_unique<SurroundProcessor>()); },
+        [] { return std::make_unique<InternalPlugin>(std::make_unique<GuitarStrummerAudioProcessor>()); }
+}
 {
 }
 
