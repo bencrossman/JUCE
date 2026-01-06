9b4e47f26ca2ea1808afce77dcfed97928085e0e
Ben Crossman <bencrossman@gmail.co>
2026-01-06 22:31:04 +1000

    Some notes

--- CopyToServer.bat
+++
@@ -1,7 +0,0 @@
-copy "extras\AudioPluginHost\Builds\VisualStudio2019\x64\Release\App\AudioPluginHost.exe" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
-copy "extras\AudioPluginHost\Ben.performer" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
-copy "extras\AudioPluginHost\Sampler.txt" "\\server\D\Data\Audio\Synth Backups\Software\Performer"
-
-
-xcopy "C:\Performance" "\\server\D\Data\Audio\Synth Backups\Performance" /s /d /y
-
